1e183aaa69f6af5b9e7f021a58411bed4ceed00e
unknown <Jim@Tempest-PC.(none)>
2011-11-21 20:45:07 -0800

    Modified the audio player to play sounds from the source.
    In addition, added functionality to get audio data for a specific frame
    in the video.  This may need tweaked when we actually go and use it.
    However, the general idea how to access the information should remain the
    same.

--- VideoPlayer/AudioPlayer.cs
+++ VideoPlayer/AudioPlayer.cs
@@ -12,15 +12,20 @@ using System.Linq;
 using System.Text;
 
 using IrrKlang;
+using System.IO;
 
 namespace VideoPlayer
 {
     class AudioPlayer
     {
         public bool isSoundLoaded;
+
+        private String soundFilePath;
+        private FileStream fileStream;
+
         private ISoundEngine engine;
-        private string soundFileName;
         private ISound sound;
+        private ISoundSource source;
 
         float volume;
 
@@ -28,6 +33,10 @@ namespace VideoPlayer
         {
             volume = 1.0f;
             engine = new ISoundEngine();
+            sound = null;
+            source = null;
+
+            soundFilePath = "";
             isSoundLoaded = false;
         }
 
@@ -35,7 +44,39 @@ namespace VideoPlayer
         {
             bool result = true;
 
-            soundFileName = filePath;
+            soundFilePath = filePath;
+
+            // Try to open a file stream for the audio file.
+            FileInfo info = new FileInfo(filePath);
+            if (info.Exists == true)
+            {
+                fileStream = null;
+                try
+                {
+                    fileStream = new FileStream(info.FullName, FileMode.Open, FileAccess.Read);
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            // Clean up on failure.
+            if (result == false && fileStream != null)
+            {
+                fileStream.Close();
+                fileStream = null;
+            }
+
+            if (result == true)
+            {
+                source = engine.AddSoundSourceFromIOStream(fileStream, filePath);
+                GetFrame(10.3f);
+            }
 
             return result;
         }
@@ -48,26 +89,74 @@ namespace VideoPlayer
         {
             try
             {
-                sound = engine.Play2D(soundFileName, false, false, StreamMode.AutoDetect, false);
+                sound = engine.Play2D(source, false, false, false);
                 sound.PlayPosition = startingPosition;
                 sound.Volume = volume;
                 isSoundLoaded = true;
             }
-            catch { }
+            catch {}
         }
 
         public void OnSetVolume(float volume)
         {
             this.volume = volume;
+
+            if (isSoundLoaded == true)
+            {
+                sound.Volume = volume;
+            }
         }
 
         public void OnStop()
         {
+            isSoundLoaded = false;
             try
             {
                 engine.StopAllSounds();
             }
             catch {}
+        }
+
+        public byte[] GetFrame(float timeInSeconds)
+        {
+            // Sanitiy
+            if (source == null)
+            {
+                return null;
+            }
+
+            byte[] result = null;
+
+            FileInfo info = new FileInfo(soundFilePath);
+            if (fileStream != null)
+            {
+                // Create a binary reader and get the data.
+                BinaryReader reader = null;
+                try
+                {
+                    reader = new BinaryReader(fileStream);
+
+                    int frameDataLength = source.AudioFormat.FrameCount * source.AudioFormat.FrameSize;
+                    long headerDataLength = info.Length - (long)frameDataLength;
+
+                    // Seek past the header and up to the current frame.
+                    int offset = (int) Math.Floor(source.AudioFormat.BytesPerSecond * timeInSeconds);
+                    long seekPosition = headerDataLength + offset;
+                    reader.BaseStream.Seek( seekPosition, SeekOrigin.Begin );
+
+                    // Read the frame
+                    result = reader.ReadBytes(source.AudioFormat.FrameSize);
+                }
+                catch {}
+
+                // Clean up
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+            return result;
         }
     }
 }
